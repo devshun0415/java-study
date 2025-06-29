ada0a5568401b4d6289c302a29c142ac7c0acddd
dev-fjk <fjk.develop@gmail.com>
2025-06-29 17:27:48 +0900

    exam1 fizzBuzz

---
+++ examination/src/FizzBuzz.java
@@ -0,0 +1,17 @@
+/**
+ * Q1: Fizzbuzz問題
+ * 
+ * (1) 1~100までの数字をコンソールに出力してください。
+ * (2) 3の倍数の時はFizzと出力して下さい
+ * (3) 5の倍数の時はBuzzと出力して下さい。
+ * (4) 3/5両方の倍数の際はFizzBuzzと出力して下さい・
+ * 
+ * 【challenge】
+ * (1) Scannerを利用して、終了値を可変にできるようにしてください。
+ * (2) 出力文面の判定ロジックをprivate static methodに切り分けてみてください。
+ */
+public class FizzBuzz {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
